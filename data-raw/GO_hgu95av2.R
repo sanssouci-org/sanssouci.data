320dc49ab278e538bef592b9e2f569fa99fa0357
Pierre Neuvial <pneuvial@gmail.com>
2020-11-15 02:02:02 +0100

    fix missing probes in GO terms

--- data-raw/GO_hgu95av2.R
+++ data-raw/GO_hgu95av2.R
@@ -13,14 +13,12 @@ nbProbes <- length(probeNames)
 pval <- dex[["p.value"]]
 adjp <- p.adjust(pval, method = "BH")
 #selected <- probeNames[which(adjp < 0.05)]
-selected <- probeNames[which(pval < 0.001)]
+selected <- probeNames[which(pval < 0.00001)]
 length(selected)
 # end data set-specific code
 
 way2GO <- function(probeNames, selected, chipType,
-                  ontology = c("CC", "BP", "MF"),
-                  minNbProbes = 5  # to only keep GO terms with 'enough' genes
-                  ) {
+                  ontology = c("CC", "BP", "MF")) {
     nbProbes <- length(probeNames)
 
     chip.db <- sprintf("%s.db", chipType)
@@ -28,19 +26,16 @@ way2GO <- function(probeNames, selected, chipType,
     envir <- eval(as.name(sprintf("%sGO", chipType)))
     gene2GO <- lapply(mget(selected, envir = envir, ifnotfound = NA),
                       names)
-    gene2GO[sapply(gene2GO, is.null)] <- NA
+    selGO <- unique(unlist(gene2GO))
+    envir <- eval(as.name(sprintf("%sGO2PROBE", chipType)))
+    go2genes <- lapply(mget(selGO, envir = envir), as.character)
 
-    go2genes <- topGO::annFUN.gene2GO(whichOnto = ontology, gene2GO = gene2GO)
-    # keep GO terms with 'enough' genes
-    ww <- which(sapply(go2genes, length) > minNbProbes)
-    go2g <- go2genes[ww]
-
-    mat <- matrix(0, nrow = nbProbes, ncol = length(go2g))
-    for (gg in seq_along(go2g)) {
-        mm <- match(go2g[[gg]], probeNames)
+    mat <- matrix(0, nrow = nbProbes, ncol = length(go2genes))
+    for (gg in seq_along(go2genes)) {
+        mm <- match(go2genes[[gg]], probeNames)
         mat[mm, gg] <- 1
     }
-    colnames(mat) <- names(go2g)
+    colnames(mat) <- names(go2genes)
     rownames(mat) <- probeNames
     mat
 }
@@ -49,13 +44,13 @@ chipType <- "hgu95av2"
 
 ## Biological process
 hgu95av2_GO_BP <- way2GO(probeNames = probeNames, selected = selected, chipType = chipType,
-      ontology = "BP", minNbProbes = 5)
+      ontology = "BP")
 str(hgu95av2_GO_BP)
-# num [1:12625, 1:83] 0 0 0 0 0 0 0 0 0 0 ...
+# num [1:12625, 1:631] 0 0 0 0 0 0 0 0 0 0 ...
 # - attr(*, "dimnames")=List of 2
 # ..$ : chr [1:12625] "1000_at" "1001_at" "1002_f_at" "1003_s_at" ...
-# ..$ : chr [1:83] "GO:0000122" "GO:0001525" "GO:0001934" "GO:0002576" ...
-hist(colSums(hgu95av2_GO_BP))
+# ..$ : chr [1:631] "GO:0000278" "GO:0001843" "GO:0001922" "GO:0001934" ...
+hist(colSums(hgu95av2_GO_BP), breaks = 100)
 
 usethis::use_data(hgu95av2_GO_BP, overwrite = TRUE, internal = FALSE)
 
--- tests/testthat/test_hgu95av2_GO_BP.R
+++ tests/testthat/test_hgu95av2_GO_BP.R
@@ -3,7 +3,7 @@ context("GO/BP annotation for the hgu95av2 microarray")
 test_that("Vanilla tests for hgu95av2_GO_BP", {
     data("hgu95av2_GO_BP")
     expect_equal(nrow(hgu95av2_GO_BP), 12625)
-    expect_equal(ncol(hgu95av2_GO_BP), 83)
+    expect_equal(ncol(hgu95av2_GO_BP), 631)
 })
 
 test_that("Match between fMRI data and brain Atlas", {
